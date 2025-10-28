e458a7659ee40be7e781b5b8ad73d3b43f6a025f
Family <mh5212357@gmail.com>
2025-10-28 15:03:51 -0400

    I completed House classes

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
-public class Vehicle {
+public class Vehicle extends Assets{
+   private String makeModel;
+   private int year;
+   private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
 }
